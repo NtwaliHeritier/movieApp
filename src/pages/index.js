9d4e4d68221312b02cfcaf0879deca2dabfb82cc
NtwaliHeritier <heritierntwali@gmail.com>
2021-04-29 14:20:48 +0200

    add state to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,44 @@
-import { graphql, useStaticQuery } from "gatsby";
-import React from "react"
+import { graphql } from "gatsby";
+import React, { useEffect, useState } from "react"
 import Layout from "./layout";
 
 import "../css/index.css";
 import Movie from "./movie";
 
-const Home = () => {
-
-  const {allTable: {edges}} = useStaticQuery(graphql`
-    query {
-      allTable {
-        edges {
-          node {
-            Name_of_the_Movie
-            Year
-            Description
-            cover
-            fields {
-          slug
-        }
+export const data = graphql`
+      query {
+        allTable {
+          edges {
+            node {
+              Name_of_the_Movie
+              Year
+              Description
+              cover
+              fields {
+            slug
+          }
+            }
           }
         }
-      }
-    }
-  `);
+      }`;
+
+const Home = ({data}) => {
+  const [nodeedges, setEdges] = useState(undefined);
+
+  useEffect( ()=> {
+    const edges = data.allTable.edges;
+    setEdges(edges);
+  }, [])
+
+  if(nodeedges === undefined) {
+    return <div>Loading...</div>
+  }
 
   return ( 
     <Layout>
       <div className="container">
         <div className="movies">
-          {edges.map(edge => <Movie edge={edge} key={edge.node.Name_of_the_Movie}/>
+          {nodeedges.map(edge => <Movie edge={edge} key={edge.node.Name_of_the_Movie}/>
             )}
         </div>
       </div>
@@ -38,3 +47,4 @@ const Home = () => {
 }
  
 export default Home;
+

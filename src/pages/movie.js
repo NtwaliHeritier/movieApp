a6839325fa533823ad0eb6bfa8deeb8e78be0100
NtwaliHeritier <heritierntwali@gmail.com>
2021-04-30 04:12:03 +0200

    update hover effect

--- src/pages/movie.js
+++ src/pages/movie.js
@@ -1,39 +1,47 @@
-import { Link } from 'gatsby';
-import React, { useEffect, useState } from 'react';
-import "../css/movie.css";
+import { Link } from "gatsby"
+import React, { useEffect, useState } from "react"
+import "../css/movie.css"
 
-const Movie = ({edge}) => {
-
-  const [pageEdge, setEdge] = useState(undefined);
+const Movie = ({ edge }) => {
+  const [pageEdge, setEdge] = useState(undefined)
 
   useEffect(() => {
-    setEdge(edge);
+    setEdge(edge)
   }, [])
 
-  const [hover, setHover] = useState(false);
+  const [hover, setHover] = useState(false)
 
   const handleEnter = () => {
-    setHover(true);
+    setHover(true)
   }
 
   const handleLeave = () => {
     setHover(false)
   }
 
-  if(pageEdge === undefined) {
+  if (pageEdge === undefined) {
     return <div>Loading...</div>
   }
 
-  return ( 
-    <div className="movieContainer" onMouseEnter={handleEnter} onMouseLeave={handleLeave}>
-            <div className={`overlay${hover? "" : " display"}`}></div>
-            <div className={`movieTitle${hover? "" : " display"}`}>
-              <h3>{edge.node.Name_of_the_Movie}</h3>
-              <Link to={`/movie/${edge.node.fields.slug}`} className="movieButton">Details</Link>
-            </div>
-            <img src={edge.node.cover} alt={edge.node.Name_of_the_Movie} className="movieImage"/>
-          </div>
-   );
+  return (
+    <div
+      className="movieContainer"
+      onMouseEnter={handleEnter}
+      onMouseLeave={handleLeave}
+    >
+      <div className={`movieTitle${hover ? "" : " display"}`}>
+        <h3>{edge.node.Name_of_the_Movie}</h3>
+        <Link to={`/movie/${edge.node.fields.slug}`} className="movieButton">
+          Details
+        </Link>
+      </div>
+      <img
+        src={edge.node.cover}
+        alt={edge.node.Name_of_the_Movie}
+        className="movieImage"
+      />
+    </div>
+  )
 }
- 
-export default Movie;
+
+export default Movie
